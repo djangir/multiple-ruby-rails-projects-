9645b7fa35d3974f897d0d7480505eec69fc99eb
dinesh jangir <djangir8875@gmail.com>
2021-08-14 22:24:10 +0530

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
-  def new
+  def index
+    @user = User.all
   end
+
+  def show 
+    begin
+      @user = User.find(params[:id])
+    rescue 
+      @user = User.find(1)
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user =  User.new(user_params) 
+    if @user.save
+      notice = "User was successfully created."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :microposts
+  resources :users
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
